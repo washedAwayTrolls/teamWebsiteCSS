fb82f1dffc96d2d8c5b587a63a05fa9ac521bb56
MrTdrake <74083273+MrTdrake@users.noreply.github.com>
2021-02-13 14:29:41 -0700

    modals

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-var modal = document.getElementById('simpleModal');
+var modals = document.querySelectorAll('.simpleModal');
 
-var modalBtn = document.getElementById('modalBtn');
+var modalBtn = document.querySelectorAll('button.modalBtn');
 
-var closeBtn = document.getElementsByClassName('timcloseBtn')[0];
+var spans = document.getElementsByClassName('timcloseBtn');
 
+for (var i = 0; i < modalBtn.length; i++) {
+    modalBtn[i].onclick = function (e) {
+        e.preventDefault();
+        modal = document.querySelector(e.target.getAttribute("href"));
+        modal.style.display = "block";
+    }
+}
+
+
+for (var i = 0; i < spans.length; i++) {
+    spans[i].onclick = function() {
+       for (var index in modals) {
+         if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+       }
+    }
+   }
+
+   window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
+
+
+/*
 modalBtn.addEventListener('click', openModal);
 
 closeBtn.addEventListener('click', closeModal);
@@ -25,3 +52,4 @@ function clickOutside(e){
     }
 
 }
+*/
